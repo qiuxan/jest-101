7fe054b162d5bae3b7045e907b2f3f1b26540a8a
ian-qiu2025 <ian.qiu@munrofg.com>
2025-03-19 12:07:00 +1100

    Extract Function

--- rot13.ts
+++ rot13.ts
@@ -1,19 +1,18 @@
 const letters = "ABCDEFGHIJKLMNOPORSTUVWXYZ";
 const separater = "";
-export const convert = (str: string) => {
-  return str
-    .split(separater)
-    .map((c) => {
-      const index = letters.indexOf(c);
-      if (index !== -1) {
-        if (index + 13 >= 26) {
-          return letters[index + 13 - 26];
-        } else {
-          return letters[index + 13];
-        }
-      }
 
-      return c;
-    })
-    .join(separater);
+const transform = (c: string) => {
+  const index = letters.indexOf(c);
+  if (index !== -1) {
+    if (index + 13 >= 26) {
+      return letters[index + 13 - 26];
+    } else {
+      return letters[index + 13];
+    }
+  }
+  return c;
+};
+
+export const convert = (str: string) => {
+  return str.split(separater).map(transform).join(separater);
 };
