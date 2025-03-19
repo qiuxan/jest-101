6e74fa01d0e3816d93a8f3590ec810a2565840c5
qiuxan <xianq@postoffice.utas.edu.au>
2025-03-19 01:53:15 +0000

    Simplify Logic

--- rot13.ts
+++ rot13.ts
@@ -1,22 +1,15 @@
 import { dict , separater, shift } from "./constants"; 
 
-const getIndex = (index: number, offset: number) => {
-  return index + offset >= 26 ? index + offset - 26 : index + offset;
-};
+const getIndex = (index: number, offset: number) => (index + offset) % dict. length;
+
 
 const getLetterWithOffset = (letter: string) => {
   const index = dict.indexOf(letter);
   return dict[getIndex(index, shift)];
 };
 
-const transform = (letter: string) => {
-  const index = dict.indexOf(letter);
-  if (index !== -1) {
-    return getLetterWithOffset(letter);
-  }
-  return letter;
-};
+const transform = (letter: string) => dict.indexOf(letter) !== -1? getLetterWithOffset(letter): letter;
 
-export const convert = (str: string) => {
-  return str.split(separater).map(transform).join(separater);
-};
+export const convert = (str: string) => str.split(separater).map(transform).join(separater);
+
+
